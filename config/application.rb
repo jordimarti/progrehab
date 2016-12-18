aa6c0cad14fbd73f55d161bfe3c0930d7afce14b
Jordi Martí Muñoz <jordimarti@me.com>
2016-12-18 18:12:12 +0100

    Configura sendgrid per enviament emails

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module Progrehab
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.action_mailer.default_url_options = { host: 'http://progrehab.herokuapp.com' }
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,11 +88,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
+   :address              => "smtp.sendgrid.net",
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :user_name            => ENV['sendgrid_username'],
+   :password             => ENV['sendgrid_password'],
    :authentication       => "plain",
-  :enable_starttls_auto => true
+   :enable_starttls_auto => true
   }
 end
