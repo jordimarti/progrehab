f1346266bd7c691695fbe4de8190a2605df2beb1
Jordi Martí Muñoz <jordimarti@me.com>
2016-12-29 00:15:16 +0100

    Modifica default mail url per development i restaura token a la vista de reset passowrd

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,7 @@ Rails.application.configure do
 
   # Configuració de Mailer
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   
   config.action_mailer.smtp_settings = {
    :address              => "smtp.sendgrid.net",
