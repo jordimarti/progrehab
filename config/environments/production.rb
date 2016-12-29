d396606d5801406b4a00117f203d91e9e8f8a621
Jordi Martí Muñoz <jordimarti@me.com>
2016-12-29 10:07:28 +0100

    Activa els errors de mailer per intentar descobrir error token recuperació contrasenya

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
   # Configuració de Mailer
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'progrehab.herokuapp.com' }
+  config.action_mailer.raise_delivery_errors = true
   
   config.action_mailer.smtp_settings = {
    :address              => "smtp.sendgrid.net",
