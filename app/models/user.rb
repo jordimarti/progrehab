1b1dc401ee1b61f7be9b9e5f558131b795f6f69f
Jordi Martí Muñoz <jordimarti@me.com>
2016-12-29 11:01:08 +0100

    Canvis en la validació de nom i cognoms

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
   after_create :subscribe
 
-  #validates :first_name, :presence => true
-  #validates :last_name, :presence => true
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
 
   def name
     [first_name, last_name].compact.join(' ')
