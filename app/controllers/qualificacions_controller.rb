ad5cee30236ac1feabeac57628f4aefe1031ead8
Jordi Martí Muñoz <jordimarti@me.com>
2017-02-19 16:00:10 +1100

    Textos qualificacions

--- app/controllers/qualificacions_controller.rb
+++ app/controllers/qualificacions_controller.rb
@@ -1,25 +1,12 @@
 class QualificacionsController < ApplicationController
+  include CheckUser
   before_action :set_qualificacio, only: [:show, :edit, :update, :destroy]
   before_action :set_edifici
 
-  # GET /qualificacions
-  # GET /qualificacions.json
-  def index
-    @qualificacions = Qualificacio.all
-  end
-
-  # GET /qualificacions/1
-  # GET /qualificacions/1.json
-  def show
-  end
-
-  # GET /qualificacions/new
-  def new
-    @qualificacio = Qualificacio.new
-  end
-
-  # GET /qualificacions/1/edit
   def edit
+    @subnavigation = true
+    @submenu_actiu = 'deficiencies'
+    check_user_edifici(@edifici.id)
     if @qualificacio.xml_file.exists?
       @file_uploaded = true
     else
@@ -27,28 +14,10 @@ class QualificacionsController < ApplicationController
     end
   end
 
-  # POST /qualificacions
-  # POST /qualificacions.json
-  def create
-    @qualificacio = Qualificacio.new(qualificacio_params)
-
-    respond_to do |format|
-      if @qualificacio.save
-        format.html { redirect_to @qualificacio, notice: 'Qualificacio was successfully created.' }
-        format.json { render :show, status: :created, location: @qualificacio }
-      else
-        format.html { render :new }
-        format.json { render json: @qualificacio.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /qualificacions/1
-  # PATCH/PUT /qualificacions/1.json
   def update
     respond_to do |format|
       if @qualificacio.update(qualificacio_params)
-        format.html { redirect_to @qualificacio, notice: 'Qualificacio was successfully updated.' }
+        format.html { redirect_to edit_qualificacio_path, notice: 'Qualificacio was successfully updated.' }
         format.json { render :show, status: :ok, location: @qualificacio }
       else
         format.html { render :edit }
@@ -57,18 +26,7 @@ class QualificacionsController < ApplicationController
     end
   end
 
-  # DELETE /qualificacions/1
-  # DELETE /qualificacions/1.json
-  def destroy
-    @qualificacio.destroy
-    respond_to do |format|
-      format.html { redirect_to qualificacions_url, notice: 'Qualificacio was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_qualificacio
       @qualificacio = Qualificacio.find(params[:id])
     end
@@ -77,7 +35,6 @@ class QualificacionsController < ApplicationController
       @edifici = Edifici.find(@qualificacio.edifici_id)
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def qualificacio_params
       params.require(:qualificacio).permit(:edifici_id, :xml_file)
     end
