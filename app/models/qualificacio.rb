e87f84f6fc90f7b1d0080d740e592a95f02bf080
Jordi Martí Muñoz <jordimarti@me.com>
2017-03-01 19:22:53 +0100

    Soluciona un problema pel qual s'intentava parsejar xml al crear qualificacions, en comptes de fer-ho només amb l'update

--- app/models/qualificacio.rb
+++ app/models/qualificacio.rb
@@ -3,7 +3,9 @@ class Qualificacio < ApplicationRecord
 	belongs_to :edifici
 	
 	has_attached_file :xml_file
-	before_save :parse_file
+	
+  before_update :parse_file
+  
 	#validates_attachment_content_type :xml_file, :content_type => ["application/xml"]
 	#No estic validant perquè els arxius xml de les ITEs no tenen header i no passen la validació
 	do_not_validate_attachment_file_type :xml_file
@@ -28,9 +30,9 @@ class Qualificacio < ApplicationRecord
         deficiencia.descripcio = node.text.to_s if node.name.eql? 'deficienciaDescripcio'
         deficiencia.observacions = node.text.to_s if node.name.eql? 'deficienciaObservacions'
         if node.name.eql? 'gravetat'
-					node.elements.each do |node|
-        		deficiencia.qualificacio = node.text.to_s if node.name.eql? 'valor'
-        	end
+          node.elements.each do |node|
+            deficiencia.qualificacio = node.text.to_s if node.name.eql? 'valor'
+          end
         end
       end
       deficiencia.save
