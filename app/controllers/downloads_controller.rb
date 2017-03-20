bb293f58a5a92b806b487ff094b14585b82459db
Jordi Martí Muñoz <jordimarti@me.com>
2017-03-20 12:28:27 +0100

    Posa creus en el quadre de deficiències!

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,6 +1,8 @@
 class DownloadsController < ApplicationController
- 
-  def show
+  # Utilitzem helper_method per tenir-lo accessible des de la vista
+  helper_method :comprova_qualificacions
+
+  def show 
     respond_to do |format|
       format.pdf { send_edifici_pdf }
 
@@ -9,7 +11,23 @@ class DownloadsController < ApplicationController
       end
     end
   end
- 
+
+  def comprova_qualificacions(sistema, qualificacio)
+    deficiencies = Deficiencia.where(edifici_id: 2, sistema: sistema)
+    if deficiencies.exists?
+      deficiencies.each do |deficiencia|
+        if deficiencia.qualificacio == qualificacio
+          return 'X'
+        else
+          return nil
+        end
+      end
+    else
+      return nil
+    end
+  end
+
+
   private
  
   def edifici_pdf
