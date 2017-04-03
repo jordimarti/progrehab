94e5236bf4078a4afbc7e65a6bec9589b28fe803
Jordi Martí Muñoz <jordimarti@me.com>
2017-04-03 09:55:03 +0200

    Prova per header pdf

--- app/models/document_pdf.rb
+++ app/models/document_pdf.rb
@@ -10,7 +10,8 @@ class DocumentPdf
  
   def to_pdf
     url_header = 'http://progrehab.herokuapp.com/edificis/pdf_header?locale=ca'
-    kit = PDFKit.new(as_html, header_html: url_header, page_size: 'A4')
+    #kit = PDFKit.new(as_html, header_html: url_header, page_size: 'A4')
+    kit = PDFKit.new(as_html, page_size: 'A4')
     kit.to_file("#{Rails.root}/public/programa.pdf")
   end
  
